8da6219ac1a6a1db5de19547453e37b11cee180b
ioanadecasmir20 <ioana@worldsecuritygroup.co.uk>
2026-03-28 22:50:45 +0000

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cpdtracker.app',
+  appName: 'CPD Tracker',
+  webDir: 'dist'
+};
+
+export default config;
