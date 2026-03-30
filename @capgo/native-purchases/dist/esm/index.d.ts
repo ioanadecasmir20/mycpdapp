ceb603d6a7807f952aadb9d5d6a280505ac45583
ioanadecasmir20 <ioana@worldsecuritygroup.co.uk>
2026-03-30 19:13:58 +0100

    Delete @capgo/native-purchases directory

--- @capgo/native-purchases/dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { NativePurchasesPlugin } from './definitions';
-declare const NativePurchases: NativePurchasesPlugin;
-export * from './definitions';
-export { NativePurchases };
--- @capgo/native-purchases/dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const NativePurchases = registerPlugin('NativePurchases', {
-    web: () => import('./web').then((m) => new m.NativePurchasesWeb()),
-});
-export * from './definitions';
-export { NativePurchases };
-//# sourceMappingURL=index.js.map
