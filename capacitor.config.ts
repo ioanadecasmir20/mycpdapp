70d756189c8e0eac9dcdd320cf794914f34cd4a3
ioanadecasmir20 <ioana@worldsecuritygroup.co.uk>
2026-03-30 18:52:05 +0100

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.cpdtracker.app',
-  appName: 'CPD Tracker',
-  webDir: 'dist'
-};
-
-export default config;
